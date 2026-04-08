1e9968ae14d103f43871f03d9bae67c1a64eaf9f
vilerareza <83896109+vilerareza@users.noreply.github.com>
2026-04-08 11:17:54 +0700

    Update inference.py for predictable output filename

--- inference.py
+++ inference.py
@@ -104,9 +104,7 @@ def main(args):
         )
         ################################### save results to output folder. ###########################################
         save_to_mp4(
-            _video_frames, 
-            f"{args.output_dir}/{os.path.basename(task.ref_video_path).split('.')[0]}" \
-            f"_{datetime.now().strftime('%Y%m%d%H%M%S')}.mp4",
+            _video_frames, os.path.join(args.output_dir, f"{os.path.basename(task.ref_video_path).split('.')[0]}.mp4"),
             fps=task.fps,
         )
 
